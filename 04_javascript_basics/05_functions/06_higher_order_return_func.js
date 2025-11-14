fd252200994d71d2fda4aa31e2704e7e5a29054c
Anjali Patel <anjalipatel.dev@gmail.com>
2025-11-14 20:19:18 +0530

    closure added

--- 04_javascript_basics/05_functions/06_higher_order_return_func.js
+++ 04_javascript_basics/05_functions/06_higher_order_return_func.js
@@ -1,7 +1,8 @@
 //? Create a teaMaker function which returns anothe function which takes parameter as teaType, give the teaType as "Green Tea".
 function teaMaker() {
+    let fevTea = "Masala Tea";
     return function(teaType) {
-        return `Here is your ${teaType}`;
+        return `Here is your ${teaType} along with my favorite ${fevTea}`;//closure
     };
 }
 const makeGreenTea = teaMaker();
