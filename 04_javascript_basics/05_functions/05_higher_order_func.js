dce7441a2d346549d588efe2702c9ac3eb89055b
Anjali Patel <anjalipatel.dev@gmail.com>
2025-11-23 10:17:28 +0530

    updated higher order function

--- 04_javascript_basics/05_functions/05_higher_order_func.js
+++ 04_javascript_basics/05_functions/05_higher_order_func.js
@@ -1,9 +1,12 @@
 //? create a function named process tea, which takes another function "makeTea" as parapmeter inside which there is an argument "masalaTea".
 function processTea(makeTea){
-    return makeTea("Masala Tea");
+    return makeTea(teaType);
 }
 function makeTea(teaType){
-    return `take your ${teaType}`;
+    return teaType();
+}
+function teaType(){
+    return "Here is your Masala Tea";
 }
 //? call the function processTea with makeTea as argument and log the result
 console.log(processTea(makeTea));
